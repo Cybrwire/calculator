5fc59f2a94171279da93119f557098b4861441f0
jmontgomery <jmontgomery786@gmail.com>
2026-03-03 16:29:39 +0900

    added checks for state on keypress

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const input = document.querySelector(".input");
-var operant1, operant2, operator;
+var operant1, operant2, operator, result;
+var currentState = "fresh";
 
+input.addEventListener('click',(event) => {
+  console.log('Target:', event.target); 
+  let key = event.target;
+    if (currentState === "fresh" && key.parentElement.classList.contains("numberpad")){
+      operant1 = key.textContent;
+      currentState = "entering_1st_operant";
+    }
+    else if (currentState === "entering_1st_operant"){
+      
+    }
+    console.log("State: ", currentState);
+});
 
-
-
-input.addEventListener('click', )
